7ae05221adb7bf64a818dbd9a791bd00beaab226
nerbertb <nerbertb@gmail.com>
2025-11-06 23:42:21 +0800

    fix: moved images to public

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,12 @@ function Card() {
     <div className="flex items-center justify-center place-content-center h-screen bg-[#F2EAE2] md:min-w-3xl">
       <div className=" max-w-[350px] md:max-w-[600px] md:flex">
         <img
-          src="./src/images/image-product-mobile.jpg"
+          src="/images/image-product-mobile.jpg"
           alt="Image Product"
           className="md:hidden rounded-t-lg"
         />
         <img
-          src="./src/images/image-product-desktop.jpg"
+          src="/images/image-product-desktop.jpg"
           alt="Image Product"
           className="hidden md:flex max-w-[300px] rounded-l-lg "
         />
@@ -33,7 +33,7 @@ function Card() {
           </div>
 
           <button className='flex justify-center items-center w-full bg-[#3D8168] hover:bg-[#1A4032] px-8 py-4 rounded-lg gap-2'>
-            <img src="./src/images/icon-cart.svg" alt="" className='size-[18px]'  />
+            <img src="/images/icon-cart.svg" alt="" className='size-[18px]'  />
             <p className='text-[14px]/[160%] text-white montserrat-medium'>Add to Cart</p></button>
         </div>
       </div>
