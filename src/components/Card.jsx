15ddeaf6ec0a5d70ed867def421a0aba2c0706e1
nerbertb <nerbertb@gmail.com>
2025-11-06 23:32:04 +0800

    fix: completed UI responsiveness on Table and Desktop

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,19 @@
 function Card() {
   return (
-    <div className="flex items-center justify-center place-content-center h-screen bg-[#F2EAE2]">
-      <div className="card-component max-w-[350px]">
+    <div className="flex items-center justify-center place-content-center h-screen bg-[#F2EAE2] md:min-w-3xl">
+      <div className=" max-w-[350px] md:max-w-[600px] md:flex">
         <img
           src="./src/images/image-product-mobile.jpg"
           alt="Image Product"
-          className="rounded-t-lg"
+          className="md:hidden rounded-t-lg"
+        />
+        <img
+          src="./src/images/image-product-desktop.jpg"
+          alt="Image Product"
+          className="hidden md:flex max-w-[300px] rounded-l-lg "
         />
 
-        <div className="bg-white p-8">
+        <div className="bg-white p-8 rounded-b-lg sm:rounded-b-none md:rounded-r-lg">
           <div className="flex flex-col gap-6">
             <h3 className="text-xs/[120%] tracking-[5px] uppercase text-[#6C7289] montserrat-medium">
               Perfume
@@ -29,7 +34,7 @@ function Card() {
 
           <button className='flex justify-center items-center w-full bg-[#3D8168] hover:bg-[#1A4032] px-8 py-4 rounded-lg gap-2'>
             <img src="./src/images/icon-cart.svg" alt="" className='size-[18px]'  />
-            <p className='text-[14px]/[160%] text-white montserrat-bold'>Add to Cart</p></button>
+            <p className='text-[14px]/[160%] text-white montserrat-medium'>Add to Cart</p></button>
         </div>
       </div>
     </div>
